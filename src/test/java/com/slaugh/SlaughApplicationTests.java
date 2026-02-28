3bb938deb6d5b8796880e3787d3817b23d28e4e8
Andrew McCormack <andrew.mccormack@gmail.com>
2026-02-28 18:27:32 +0000

    Initial commit
    - Set up Spring Boot foundation with Flyway and Modulith

---
+++ src/test/java/com/slaugh/SlaughApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slaugh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlaughApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
